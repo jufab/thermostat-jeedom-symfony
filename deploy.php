da61ab4ca7d473e73c1a36c1e966e9dd45acf272
Julien FABRE <julien.fabre.33@gmail.com>
2019-09-11 00:29:11 +0200

    fix: deploy

--- deploy.php
+++ deploy.php
@@ -5,41 +5,29 @@ require 'recipe/symfony4.php';
 
 // Project name
 set('application', 'thermostat-jeedom-symfony');
-
 // Project repository
 set('repository', 'git@github.com:jufab/thermostat-jeedom-symfony.git');
-
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', false);
-
+set('keep_releases', 1);
 // Shared files/dirs between deploys 
 //add('shared_files', []);
 //add('shared_dirs', []);
-
 // Writable dirs by web server 
 //add('writable_dirs', []);
 set('writable_mode', 'chmod');
 set('http_user', 'http');
 set('writable_chmod_mode','0777');
-
 //set('writable_use_sudo', true);
 set('allow_anonymous_stats', false);
-
 // Hosts
-
 set('default_stage', 'prod');
 host('192.168.2.12')
     ->user('admin')
     ->stage("prod")
     ->port(2222)
     ->set('deploy_path', '/volume2/web/thermostat');
-    
 // Tasks
-
-task('build', function () {
-    run('cd {{release_path}} && build');
-});
-
 /*task('deploy:droit', 'chmod -Rf 777 var/');
 after('deploy', 'deploy:droit');*/
 
